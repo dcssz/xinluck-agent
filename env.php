ba65f2da6c5de266802f6538dc51f6173b720c16
root <root@jmmdb3-va20250910.asia-east1-c.c.dcs001.internal>
2026-02-13 23:06:19 +0800

    env

--- env.php
+++ env.php
@@ -1,7 +1,7 @@
 <?php
 
 date_default_timezone_set('PRC');
-ini_set('display_errors', 'on');
-error_reporting(-1);
+ini_set('display_errors', 'off');
+error_reporting(0);
 
 define('BASE_URL', 'https://' . $_SERVER['HTTP_HOST']);
